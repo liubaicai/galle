ca52e361a6680dc7a79046e4abe4b74bc499a0a5
Baicai <liushuai.baicai@hotmail.com>
2019-01-22 13:34:17 +0800

    update ui

--- app/controllers/control_panel_controller.rb
+++ app/controllers/control_panel_controller.rb
@@ -5,6 +5,7 @@ class ControlPanelController < ApplicationController
 
     def board
         @local_server_status = Machine.status.to_json
+        @publishers = Publisher.where(published: false).order(id: :asc).take(5)
+        @logs = Log.order(id: :desc).take(10)
     end
 end
-  
