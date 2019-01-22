fa8a885db791ec89445de440e92e606418fb4fd1
刘白菜 <liushuai.baicai@hotmail.com>
2019-01-22 09:35:23 +0800

    update index

--- app/controllers/control_panel_controller.rb
+++ app/controllers/control_panel_controller.rb
@@ -1,10 +1,10 @@
 class ControlPanelController < ApplicationController
     def index
-        @local_server_status = Machine.status.to_json
+
     end
 
     def board
-        
+        @local_server_status = Machine.status.to_json
     end
 end
   
