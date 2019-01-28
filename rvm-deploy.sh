4c228228fc62b14ef3a2cc71fefdc74f500c6864
Baicai <liushuai.baicai@hotmail.com>
2019-01-28 18:36:19 +0800

    update deploy script

--- rvm-deploy.sh
+++ rvm-deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-rvm use 2.6.0
-bundle install
-rails db:migrate RAILS_ENV=production
-rails db:seed RAILS_ENV=production
-rails assets:precompile
+rvm use 2.6.0 do bundle install
+rvm use 2.6.0 do rails db:migrate RAILS_ENV=production
+rvm use 2.6.0 do rails db:seed RAILS_ENV=production
+rvm use 2.6.0 do rails assets:precompile
