060e7747e7f1f7cb0d79e92dd20b4e98cff5d51b
Baicai <liushuai.baicai@hotmail.com>
2019-01-29 14:44:12 +0800

    update js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def isDev
+        environment = ENV.fetch("RAILS_ENV") { "development" }
+        if environment=='production'
+            return false
+        end
+        return true
+    end
+
 end
