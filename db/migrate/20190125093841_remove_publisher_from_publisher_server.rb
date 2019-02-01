ef0b8157c8387ed01e90878c808ba2f0900ee2de
刘白菜 <liushuai.baicai@hotmail.com>
2019-02-01 18:03:32 +0800

    fix migration bug

--- db/migrate/20190125093841_remove_publisher_from_publisher_server.rb
+++ db/migrate/20190125093841_remove_publisher_from_publisher_server.rb
@@ -1,6 +1,6 @@
 class RemovePublisherFromPublisherServer < ActiveRecord::Migration[5.2]
   def change
-    remove_reference :publisher_servers, :publisher, foreign_key: true
+    remove_reference :publisher_servers, :publisher
     add_reference :publisher_servers, :project, index: true, foreign_key: true
   end
 end
